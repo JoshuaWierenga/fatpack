62f2b36cc1259d3437758653bcd1a0ceae6a665f
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2023-07-17 01:56:14 +1000

    Add command line sample
    The method of getting terminal access is slightly flawed as the loader program also uses the windows subsystem which causes the calling terminal to not wait for it to quit to print the prompt which conflicts with the output of this program.

--- Sample/main.c
+++ Sample/main.c
@@ -1,13 +1,25 @@
 /* Copyright (c) 2018, Sijmen J. Mulder. See LICENSE.md. */
 
+#define USE_TERM
+
 #include <windows.h>
 #include <tchar.h>
 
 int CALLBACK
-WinMain(HINSTANCE instance, HINSTANCE prev, LPSTR cmdline, int cmdshow)
-{
-	MessageBox(NULL, _T("Hello from " PLATFORM "!"), _T("Sample"),
-	    MB_OK | MB_ICONINFORMATION);
+WinMain(HINSTANCE instance, HINSTANCE prev, LPSTR cmdline, int cmdshow) {
+#ifdef USE_TERM
+	const TCHAR* output = _T("Hello from " PLATFORM "!\r\n");
 
+	if (!AttachConsole(ATTACH_PARENT_PROCESS)) {
+		AllocConsole();
+	}
+
+	WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), output,
+
+		(DWORD)_tcslen(output), NULL, NULL);
+#else
+	MessageBox(NULL, _T("Hello from " PLATFORM "!"), _T("Sample"),
+		MB_OK | MB_ICONINFORMATION);
+#endif
 	return 0;
 }
